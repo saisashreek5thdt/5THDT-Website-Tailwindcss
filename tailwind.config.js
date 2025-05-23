12f5585726d660b88654a8d84cdb1ab122b18d8e
Sai Sashreek D <saisashreekd@gmail.com>
2025-05-23 15:19:25 +0530

    Added Custom Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./\**/*.{html,js}"];
+export const content = [".//**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  extend: {
+    fontFamily: {
+      Molgan: ["Molgan"],
+      QueensideR: ["Queenside-Regular"],
+      QueensideL: ["Queenside-Light"],
+      QueensideM: ["Queenside-Medium"],
+      QueensideB: ["Queenside-Bold"],
+      Jost: ["Jost"]
+    },
+  },
 };
 export const plugins = [];
